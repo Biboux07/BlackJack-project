cddfbd3660af8695c809e6bf72455d92899e723b
Biboux07 <72875227+Biboux07@users.noreply.github.com>
2020-10-30 22:15:39 +0100

    Add files via upload
    Première version utilisable
    Possibilité de connaitre notre résultat, celui du CPU et d'avoir le Gagnant

---
+++ BlackJack_PlayerVSComputer/CPU.py
@@ -0,0 +1,40 @@
+#importations ;
+
+import random
+import DefFunctions
+
+#CPU Function.
+
+def CPUchoice ():
+    #idee d'une liste qui retiens les coups
+    #possibilité de connaitre le nombre de coups
+    k = 0
+    i = 0
+    Numfinal = 0
+    while (True) :
+
+        if Numfinal <= 14 :
+            Numcard = DefFunctions.Rcard()
+            Numfinal += Numcard
+
+            i += 1
+
+        if Numfinal >= 18 :
+            break
+
+        if Numfinal >=15 and Numfinal <=17 :
+            k = int(random.randrange(0,2))
+                
+        if k == 0 :
+            Numcard = DefFunctions.Rcard()
+            Numfinal += Numcard
+
+            i += 1
+
+        if k == 1 or k == 2 :
+             break
+
+        
+
+    CPUscore = Numfinal
+    return CPUscore
---
+++ BlackJack_PlayerVSComputer/Main.py
@@ -0,0 +1,23 @@
+import CPU
+import DefFunctions
+import Interactions
+
+
+# Actual Main #
+
+#Calculations
+
+a = Interactions.Interaction()
+
+b = CPU.CPUchoice()
+
+
+
+#Print the results
+print ("CPU:", b)
+print ("Player:", a)
+
+DefFunctions.results(a, b)
+
+
+
